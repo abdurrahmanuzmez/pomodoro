f22d417a12665b2f7b7bd15005bde2b81d4dfd17
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-07-01 07:14:07 +0300

    Init pls88

--- src/inc/transcription-form.js
+++ src/inc/transcription-form.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export class TranscForm extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            task: '',
+            time: ''
+        }
+    }
+/*
     addTranscription(e){
         e.preventDefault();
         const inp = document.getElementById('transcInput');
@@ -12,16 +20,30 @@ export class TranscForm extends React.Component{
         inpTime.value = '';
         this.props.addTranscription(val, valTime);
     }
+    //e =>this.addTranscription(e)
+*/
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value })
+
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+    }
 
     render() {
+        const {task, time} = this.state;
+
+
         return(
             <div>
                 <div className="form">
-                    <form className="input-wrapper" onSubmit={e =>this.addTranscription(e)}>
-                        <input id="transcInput" className="input" type="text" placeholder="Text here..."/>
-                        <input id="transcTimeInput" className="input" type="number" placeholder="Time here (minute)" />
+                    <form className="input-wrapper" onSubmit={this.submitHandler}>
+                        <input id="transcInput" className="input" type="text" placeholder="Text here..." name="text" value={task} onChange={this.changeHandler}/>
+                        <input id="transcTimeInput" className="input" type="number" placeholder="Time here (minute)" name="time" value={time} onChange={this.changeHandler}/>
                     </form>
-                    <button type="button" className="add-btn" onClick={e => this.addTranscription(e)}>C'mon</button>
+                    <button type="submit" className="add-btn" onClick={e => this.addTranscription(e)}>C'mon</button>
                 </div>
             </div>
         )
