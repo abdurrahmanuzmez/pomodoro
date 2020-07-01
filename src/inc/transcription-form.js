8ce14dfe8c73e98b1e58762773f061e92f7b0dfe
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-07-01 08:33:55 +0300

    abi yapma abi

--- src/inc/transcription-form.js
+++ src/inc/transcription-form.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 
 export class TranscForm extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            task: '',
-            time: ''
+            product_name: '',
+            product_price: ''
         }
     }
 /*
@@ -30,18 +31,25 @@ export class TranscForm extends React.Component{
     submitHandler = (e) => {
         e.preventDefault();
         console.log(this.state)
+        axios.post("https://arcane-reef-06137.herokuapp.com/api/products", this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
     }
 
     render() {
-        const {task, time} = this.state;
+        const {product_name, product_price} = this.state;
 
 
         return(
             <div>
                 <div className="form">
                     <form className="input-wrapper" onSubmit={this.submitHandler}>
-                        <input id="transcInput" className="input" type="text" placeholder="Text here..." name="text" value={task} onChange={this.changeHandler}/>
-                        <input id="transcTimeInput" className="input" type="number" placeholder="Time here (minute)" name="time" value={time} onChange={this.changeHandler}/>
+                        <input id="transcInput" className="input" type="text" placeholder="Text here..." name="text" value={product_name} onChange={this.changeHandler}/>
+                        <input id="transcTimeInput" className="input" type="number" placeholder="Time here (minute)" name="time" value={product_price} onChange={this.changeHandler}/>
                     </form>
                     <button type="submit" className="add-btn" onClick={e => this.addTranscription(e)}>C'mon</button>
                 </div>
