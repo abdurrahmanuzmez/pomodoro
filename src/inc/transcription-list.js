65fac526ee2bdbf04f6eccc2caf092048d1bdd54
abdurrahmanuzmez <abdurrahmanuzmez@outlook.com>
2020-07-01 08:53:58 +0300

    oylan

--- src/inc/transcription-list.js
+++ src/inc/transcription-list.js
@@ -1,6 +1,24 @@
 import React from "react";
+import axios from 'axios';
 
 export class TranscList extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            posts : []
+        };
+    }
+
+    componentDidMount() {
+        axios.get("https://arcane-reef-06137.herokuapp.com/api/products/")
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+    }
 
     removeTranscription(e){
         this.props.removeTranscription(e.target.parentNode.id);
